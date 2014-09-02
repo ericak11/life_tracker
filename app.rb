4e2259248f8b4b5003b53ae2d2e2c291beac6d7c
ericak11 <kantor.erica@gmail.com>
2014-09-02 15:26:41 -0400

    redis to go

--- app.rb
+++ app.rb
@@ -6,9 +6,10 @@ class App < Sinatra::Base
 
   configure do
     REDIS_URL = "redis://127.0.0.1:6379/0"
+    TOGO_URL = "redis://redistogo:ca40496fa8b0a88dad3837fb0c8026bf@barreleye.redistogo.com:10179/"
     enable :logging
     enable :method_override
-    $redis = Redis.new(url: REDIS_URL)
+    $redis = Redis.new(url: TOGO_URL)
     # @@coffee = 0
     # @@tea = 0
     # @@water = 0
